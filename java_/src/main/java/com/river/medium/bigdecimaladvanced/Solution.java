7aba16edb826a36fcfa05c9d149645ca0001a86d
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-10-03 18:31:04 +0800

    added

---
+++ java_/src/main/java/com/river/medium/bigdecimaladvanced/Solution.java
@@ -0,0 +1,28 @@
+package com.river.medium.bigdecimaladvanced;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        Arrays.sort(s, 0, n, (a, b) -> {
+            BigDecimal x = new BigDecimal(a);
+            BigDecimal y = new BigDecimal(b);
+            return y.compareTo(x);
+        });
+
+        //Output
+        for(int i=0;i<n;i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
